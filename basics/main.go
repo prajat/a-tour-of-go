98ebcbd23a00e45437f349f8aa4056efec9d582f
Rajat Singh <rajatsingh@Rajats-MacBook-Pro.local>
2025-12-29 12:39:07 +0530

    Added - Type Inerfaces etc.

--- basics/main.go
+++ basics/main.go
@@ -3,23 +3,55 @@ package main
 import (
 	"fmt"
 	"math"
+	"math/rand"
 )
 
 func main() {
-	//functions
+	//Functions
 	fmt.Println(add(42, 13))
 
-	//multiple-results
+	//Functions Continued
+	fmt.Println(add2(10, 90))
+
+	//Multiple Results
 	a, b := swap("hello", "world")
 	fmt.Println(a, b)
 
-	//exported-names
+	//Exported Names
 	fmt.Println(math.Pi)
+	fmt.Println(rand.Intn(10))
 
-	//naked-return
+	//Naked Return
 	fmt.Println(split(54))
 
-	//variables
-	var i int
-	fmt.Println(i, c, python, java)
+	//Variables
+	//var i int
+	//var c, python, java = true, false, "no!"
+
+	//Short Variables Declaration
+	k := 3
+	c, python, java := true, false, "no!"
+	fmt.Println(i, j, k, c, python, java)
+
+	//Basic Types
+	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
+	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
+	fmt.Printf("Type: %T Value: %v\n", Z, Z)
+
+	//Zero Values
+	fmt.Printf("%v %v %v %q\n", I, F, B, S)
+	//fmt.Printf("%T %T %T %T\n", I, F, B, S)var x, y int = 3, 4
+
+	//Type Conversions
+	var x, y int = 3, 4
+	var f float64 = math.Sqrt(float64(x*x + y*y))
+	var z uint = uint(f)
+	fmt.Println(x, y, z)
+
+	//Type Interface
+	//v := 42.6 + 34.89i  // -> complex128
+	//v := 42            // -> int
+	v := 42.66    // -> float64
+	K := int32(v) // -> int32
+	fmt.Printf("v is of type %T, %T, value of K -> %v\n", v, K, K)
 }
--- basics/variables.go
+++ basics/variables.go
@@ -7,4 +7,21 @@
 
 package main
 
-var c, python, java bool
+//var c, python, java bool
+
+/*
+Variables with initializers
+A var declaration can include initializers, one per variable.
+
+If an initializer is present, the type can be omitted; the variable will take the type of the initializer.
+*/
+
+var i, j int = 1, 2
+var c, python, java = true, false, "no!"
+
+/*
+Short variable declarations
+Inside a function, the := short assignment statement can be used in place of a var declaration with implicit type.
+
+Outside a function, every statement begins with a keyword (var, func, and so on) and so the := construct is not available.
+*/
